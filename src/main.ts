31c7dd50312097c6f7b53f2bc7211623dfefb54c
Gil Palma <gilpalma500@gmail.com>
2017-11-24 11:43:10 +0000

    installed teDB

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import * as tedb from "tedb";
+import { Datastore, IDatastore, Cursor, Ioptions, IindexOptions,
+ IupdateOptions, Index, IIndex, IStorageDriver, IRange, range,
+ isEmpty, getDate, compareArray, rmObjDups, getPath, Base64,
+ expandObj, compressObj, flatten, saveArrDups, getDups, rmArrDups} from "tedb";
+
 if (environment.production) {
   enableProdMode();
 }
